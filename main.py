ebea306383b6e9517b5f2ab36fac2f3a8811f6ca
t3i8m <number.38.tj@gmail.com>
2025-04-28 18:18:27 +0200

    changed the mnist_loader, output: (28x28) nor (784x1)

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from model.CNN import CNN
 import numpy as np
-from .model.FFNN.NN import NN
+from model.FFNN.NN import NN
 from utils.mnist_loader import load_data_wrapper
-
+import matplotlib.pyplot as plt
 
 def main():
     cnn = CNN(layers=[[3, 8], [3, 16]], first_in_channels=1)
     dummy_img = np.zeros((28, 28))
-    flat_len = cnn.feed_forward(dummy_img)
-    print("flat =", len(flat_len))  #400
+    flat_len = len(cnn.feed_forward(dummy_img))
+    print("flat =", flat_len)  #400
     nn = NN([flat_len, 30, 10])
     training_data , validation_data , test_data = load_data_wrapper()
     print("------------Data was loaded------------------")
-    print(training_data)
+    print(test_data[0])
+
+    image, label = test_data[0]
+    plt.imshow(image, cmap='gray')  
+    plt.title(f"Label: {label}")
+    plt.colorbar()
+    plt.show()
 
 
 
